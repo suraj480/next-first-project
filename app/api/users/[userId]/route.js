685eb9c503a11705e25aa598f431c3744a9401e6
suraj480 <surajmaurya991@gmail.com>
2023-10-20 07:22:15 +0530

    dyanmic routes

--- app/api/users/[userId]/route.js
+++ app/api/users/[userId]/route.js
@@ -1,6 +1,9 @@
 import { NextResponse } from "next/server";
 //DELETE request function
-export function DELETE(request) {
+export function DELETE(request,{params}) {
+    console.log(params)
+    const {userId} = params
+    console.log(userId)
     console.log("delete api called")
     return NextResponse.json({
       message:"Testing delete",
